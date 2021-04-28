80af39796abae1602135f1079a80842d3eb29334
makhatadze <vitomaxatadze@gmail.com>
2021-04-28 16:03:32 +0400

    Make event model.

---
+++ src/models/Event.tsx
@@ -0,0 +1,32 @@
+import {Ticket} from "./Ticket";
+
+
+export class Event {
+    protected id: number;
+    protected tickets: Array<Ticket>;
+
+    constructor(id: number, tickets: Array<Ticket>) {
+        this.id = id;
+        this.tickets = tickets;
+    }
+
+    hasTicket(): boolean {
+        // Return true if there is at least one ticket in this event; Otherwise, return false;
+        return this.tickets.length > 0;
+    }
+
+
+    getCheapestTicketPrice(): string {
+        // Sort the tickets in ascending order by the price of the tickets
+        let cheapestTicketPrice = '';
+        if (this.tickets.length > 0) {
+            this.tickets.sort(function (ticketA: Ticket,ticketB: Ticket) {
+                return ticketA.price - ticketB.price;
+            });
+            cheapestTicketPrice = `&${this.tickets[0].price}`
+        } else {
+            // There is no tickets in this event
+        }
+        return cheapestTicketPrice;
+    }
+}
