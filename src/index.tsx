037a57a0303daf33344ebd9130d6641600b09261
makhatadze <vitomaxatadze@gmail.com>
2021-04-28 15:46:20 +0400

    Add function for generate random double.

--- src/index.tsx
+++ src/index.tsx
@@ -18,3 +18,14 @@ ReactDOM.render(
 const generateRandomInteger = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+/**
+ * @desc Generates random double number with two decimal places
+ * @param min
+ * @param max
+ */
+const generateRandomDouble = (min: number, max: number) => {
+    return Math.floor((Math.random() * (max - min + 1) + min) * 100) / 100;
+}
+
+
