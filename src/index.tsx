4ddd97b1b7763264b41f907a4fcb64855d8611de
makhatadze <vitomaxatadze@gmail.com>
2021-04-28 15:44:25 +0400

    Add function for generate random integer.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+
+/**
+ * @desc Generates random integer
+ * @param min
+ * @param max
+ */
+const generateRandomInteger = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
