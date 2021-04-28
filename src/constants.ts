f79e46916071d1f944233c8333939828a875569a
makhatadze <vitomaxatadze@gmail.com>
2021-04-28 15:39:36 +0400

    Export constants.

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const RANGE_OF_X_AXIS = 21; // Requirement: a world that ranges from -10 to +10 (X axis)
+export const RANGE_OF_Y_AXIS =21; // Requirement: a world that ranges from -10 to +10 (Y axis)
+
+export const MAX_NUMBER_OF_LOCATIONS = 441; // Coordinates 21*21
+export const MIN_NUMBER_OF_LOCATIONS = 0;
+
+export const MAX_NUMBER_OF_TICKETS = 100; // Maximum number of tickets for one event is 100.
+export const MIN_NUMBER_OF_TICKETS = 0; // Each event has zero or more tickets.
+
+export const MAX_TICKET_PRICE = 1000; // Maximum price for one ticket is 1000 US dollars.
+export const MIN_TICKET_PRICE = 0.01; // Each ticket has a non-zero price. Assumption: the minimum ticket price is 0.01 US dollars.
+
+export const NUMBER_OF_CLOSEST_LOCATIONS = 5; // Returns a list of the five closest events
