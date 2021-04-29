207d7f9dc4570c97e33e7fa4fb3ebe0f264098fe
makhatadze <vitomaxatadze@gmail.com>
2021-04-29 09:21:24 +0400

    Export GridTable component.

--- src/components/Grid/GridTable/GridTable.tsx
+++ src/components/Grid/GridTable/GridTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {FC} from "react";
 import {Location} from "../../../models/Location";
 
 export interface gridTableProps {
@@ -7,3 +7,53 @@ export interface gridTableProps {
     inputValueX: number,
     inputValueY: number
 }
+
+
+const GridTable: FC<gridTableProps> = ({locations, isSearchedResults, inputValueX, inputValueY}) => {
+    return (
+        <div className='grid-table'>
+            <table>
+                <tr>
+                    <th>Event</th>
+                    <th>Location</th>
+                    {isSearchedResults ? (
+                        <th>Distance from ({inputValueX},{inputValueY})</th>
+                    ) : null}
+                    <th>Price from</th>
+                </tr>
+                {locations.map((location, i) => (
+                    <tr>
+                        <td>
+                            <h4>
+                                Event {location.event.id}
+                            </h4>
+                        </td>
+                        <td>
+                            ({location.coordinateX}, {location.coordinateY})
+                        </td>
+                        {isSearchedResults ? (
+                            <td>
+                                {location.getDistance(inputValueX, inputValueY)}
+                            </td>
+                        ) : null}
+                        <td>
+                            <div className='grid-table-price'>
+                                {location.event.hasTicket() ? (
+                                    <div className='grid-table-price-active'>
+                                        {location.event.getCheapestTicketPrice()}
+                                    </div>
+                                ) : (
+                                    <div className='grid-table-price-inactive'>
+                                        Sold out
+                                    </div>
+                                )}
+                            </div>
+                        </td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+}
+
+export default GridTable;
