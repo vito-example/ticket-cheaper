368fb044c4052b4b250ca426e48d19cc2e75184d
makhatadze <vitomaxatadze@gmail.com>
2021-04-29 09:31:09 +0400

    GenerateRandom elements move into utils functions.

---
+++ src/utils/generateRandom.ts
@@ -0,0 +1,92 @@
+import {
+    MAX_NUMBER_OF_LOCATIONS,
+    MAX_NUMBER_OF_TICKETS, MAX_TICKET_PRICE,
+    MIN_NUMBER_OF_LOCATIONS,
+    MIN_NUMBER_OF_TICKETS, MIN_TICKET_PRICE
+} from "../constants";
+import {Location} from "../models/Location";
+import {Event} from "../models/Event";
+import {Ticket} from "../models/Ticket";
+
+/**
+ * @desc Generates a random of locations which contains one event
+ * @return {Array<Location>} - An array of locations
+ */
+export const generateRandomLocations = () => {
+    let locations = [];
+
+    // Generate the number of locations which have event
+    let numberOfLocations: number = generateRandomInteger(MIN_NUMBER_OF_LOCATIONS,MAX_NUMBER_OF_LOCATIONS);
+    let locationIdArray: number[] = generateRandomNumberArray(numberOfLocations);
+
+    // Load the locations with one event
+    for( let i =0; i < numberOfLocations; i++) {
+        let locationID = locationIdArray[i];
+        let coordinateX = -10 + locationID%21;
+        let coordinateY = 10 - Math.floor(locationID/21);
+        let event = generateRandomEvent(i);
+        let location = new Location(locationID,coordinateX,coordinateY,event);
+        locations.push(location);
+    }
+
+    return locations;
+}
+
+/**
+ * @desc Generates a event with random number of tickets
+ * @param id
+ */
+const generateRandomEvent = (id: number) => {
+    let tickets = generateRandomTickets();
+    return  new Event(id, tickets);
+}
+
+/**
+ * @desc Generates random number of tickets with random prices.
+ * @return {Array<Ticket>} - An array of Tickets.
+ */
+const generateRandomTickets = () => {
+    // Generate a random number for the number of tickets
+    let numberOfTicket: number = generateRandomInteger(MIN_NUMBER_OF_TICKETS,MAX_NUMBER_OF_TICKETS);
+    let tickets: any[] = [];
+    for (let i=0; i < numberOfTicket; i++) {
+        // Define the price of the ticket
+        let ticket = new Ticket(generateRandomDouble(MIN_TICKET_PRICE,MAX_TICKET_PRICE));
+        tickets.push(ticket);
+    }
+
+    return tickets;
+}
+
+/**
+ * @desc Generates random integer
+ * @param min
+ * @param max
+ */
+const generateRandomInteger = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+/**
+ * @desc Generates random double number with two decimal places
+ * @param min
+ * @param max
+ */
+const generateRandomDouble = (min: number, max: number): number => {
+    return Math.floor((Math.random() * (max - min + 1) + min) * 100) / 100;
+}
+
+/**
+ * Generates an array of random exclusive location ids
+ * @param maxLength
+ */
+const generateRandomNumberArray = (maxLength: number) => {
+    let randomArray: number[] = [];
+    while (randomArray.length < maxLength) {
+        let randomNumber = Math.floor(Math.random() * MAX_NUMBER_OF_LOCATIONS);
+        if (randomArray.indexOf(randomNumber) > -1) continue;
+        randomArray[randomArray.length] = randomNumber;
+    }
+
+    return randomArray;
+}
